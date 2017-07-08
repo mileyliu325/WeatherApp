ada04c3d646462d548a0be6df72104b377c8f351
mileyliu325 <smileyliu325@gmail.com>
2017-07-08 13:11:24 +1000

    detail page

--- MLWeatherApp/DetailViewController.swift
+++ MLWeatherApp/DetailViewController.swift
@@ -11,14 +11,49 @@ import UIKit
 class DetailViewController: UIViewController {
     var sendData : Any?
 
+    @IBOutlet weak var iconImageView: UIImageView!
 
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+    @IBOutlet weak var highTempLabel: UILabel!
+    
+    @IBOutlet weak var lowTempLabel: UILabel!
+    
+    @IBOutlet weak var humidityLabel: UILabel!
+    
+    @IBOutlet weak var pressureLabel: UILabel!
+    
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if((sendData) != nil){
-//            drawUI()
+            drawUI()
         }
     }
-
+    func drawUI() ->Void{
+        
+        
+        let weather = sendData as! Weather
+        print("detail:\(weather.icon)")
+        
+        self.title =  changeUTCtoDate(UTCString: weather.time)
+        
+        iconImageView.image = UIImage(named: imageCase(iconString: weather.icon))
+        
+        summaryLabel.text = String("Summary:\(weather.summary)")
+        
+        highTempLabel.text = String("Highest Tempature: \(Int(weather.temperatureMax))°F")
+        
+        lowTempLabel.text = String("Lowest Tempature: \(Int(weather.temperatureMin))°F")
+        
+        humidityLabel.text = String("Humidity: \(Int(weather.humidity*100))%")
+        
+        pressureLabel.text = String("Pressure: \(Int(weather.pressure))hPa")
+        
+        windSpeedLabel.text = String("Wind Speed : \(Int(weather.windSpeed))m/s")
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
