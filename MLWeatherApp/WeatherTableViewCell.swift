d56909ba6f00111e5e4a5b96cf32928c49c05a7a
mileyliu325 <smileyliu325@gmail.com>
2017-07-08 11:46:36 +1000

    add basic files

---
+++ MLWeatherApp/WeatherTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherTableViewCell.swift
+//  MLWeatherApp
+//
+//  Created by Simeng Liu on 8/07/2017.
+//  Copyright © 2017 Simeng Liu. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    @IBOutlet weak var iconImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
